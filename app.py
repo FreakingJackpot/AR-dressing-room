57fe7c598943d8ab17e1d6e8081fe2ff00ab4969
FreakingJackpot <70316335+FreakingJackpot@users.noreply.github.com>
2021-01-14 02:46:29 +0400

    Конвертация byte image в image

--- app.py
+++ app.py
@@ -1,15 +1,18 @@
 import numpy
+import os
+import io
 from flask_cors import CORS, cross_origin
-from flask import Flask
+from flask import Flask,request
 from flask import jsonify
-
+from PIL import Image
 app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
-
-@app.route("/s" )
+@app.route("/s",methods=['GET', 'POST'])
 @cross_origin()
 def main():
+    data=request.get_json()
+    image=Image.open(io.BytesIO(bytearray(data['image'])))
     lpoints=numpy.load('joint_location.npy')
     rpoints=numpy.load('joint_rotation.npy')
 
