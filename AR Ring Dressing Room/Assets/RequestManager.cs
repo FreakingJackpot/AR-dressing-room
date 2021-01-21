cfdc6d1cddeff58ed141106c7599e3443cb7b949
FreakingJackpot <70316335+FreakingJackpot@users.noreply.github.com>
2021-01-21 14:19:55 +0400

    камера на opencv

--- AR Ring Dressing Room/Assets/RequestManager.cs
+++ AR Ring Dressing Room/Assets/RequestManager.cs
@@ -1,100 +1,169 @@
 ﻿using SimpleJSON;
-using System.Collections;
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
 using UnityEngine;
-using UnityEngine.Networking;
 using UnityEngine.UI;
-
 public class RequestManager : MonoBehaviour
 {
-    private string url = "";
-    public RawImage rawImage;
+    Socket socket;
+    IPAddress iP;
+    IPEndPoint iPEnd;
+    Thread thread;
+
+    bool changePic;
+
+    public byte[] ImageSize;
+    public int Len;
+
+    public byte[] Image;
+
+    byte[] Transform;
+    JSONNode data;
+
     public int posePreset = 0;
     public int rotationPreset = 0;
+
     public GameObject canvas;
+    public RawImage rawImage;
     public Transform ring;
-    WebCamTexture webcamTex;
-    Texture2D texture2D;
+
     public Text pose;
     public Text rot;
+
     string a = "Текущая позиция: ";
     string b = "Текущая ротация: ";
 
-    public class Data
+    void InitSocket()
     {
-        public byte[] image;
+        iP = IPAddress.Parse("127.0.0.1");
+        iPEnd = new IPEndPoint(iP, 9000);
+
+        thread = new Thread(new ThreadStart(SocketReceive));
+        thread.Start();
+    }
+
+    void SocketConnect()
+    {
+        if (socket != null)
+            socket.Close();
+
+        socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        socket.Connect(iPEnd);
+    }
+
+    void SocketReceive()
+    {
+        SocketConnect();
+
+        while (true)
+        {
+            try
+            {
+                ImageSize = new byte[1024];
+                Len = socket.Receive(ImageSize);
+                if (Len == 0)
+                {
+                    SocketConnect();
+                    continue;
+                }
+                else
+                {
+                    int recv_len = Convert.ToInt32(Encoding.ASCII.GetString(ImageSize, 0, Len));
+                    Image = new byte[recv_len];
+
+                    int img_len = socket.Receive(Image);
+                    if (recv_len != img_len)
+                    {
+                        Debug.Log("Битый кадр был отправлен");
+                    }
+                    else
+                    {
+                        changePic = true;
+                        Transform = new byte[4096];
+                        socket.Receive(Transform);
+
+                        string bitString = System.Text.Encoding.UTF8.GetString(Transform);
+                        data = JSON.Parse(bitString);
+                    }
+                }
+            }
+            catch
+            {
+                continue;
+            }
+        }
+    }
+
+    private void SocketClose()
+    {
+        if (thread != null)
+        {
+            thread.Interrupt();
+            thread.Abort();
+        }
+        if (socket != null)
+            socket.Close();
+        Debug.Log("Отключение от сервера");
+    }
+
+    private void Start()
+    {
+        rawImage.texture = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, false);
+        pose.text = a + posePreset.ToString();
+        rot.text = b + rotationPreset.ToString();
+        InitSocket();
     }
 
     void Update()
     {
+        if (changePic)
+        {
+            var texture = rawImage.texture as Texture2D;
+            texture.LoadImage(Image);
+            texture.Apply();
+            SetRingTransform(data);
+            changePic = false;
+        }
 
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
             posePreset++;
             pose.text = a + posePreset.ToString();
-            rot.text = b + rotationPreset.ToString();
         }
         else if (Input.GetKeyDown(KeyCode.DownArrow))
         {
             posePreset--;
             pose.text = a + posePreset.ToString();
-            rot.text = b + rotationPreset.ToString();
         }
         else if (Input.GetKeyDown(KeyCode.RightArrow))
         {
-            pose.text = a + posePreset.ToString();
-            rot.text = b + rotationPreset.ToString();
+
             rotationPreset++;
+            rot.text = b + rotationPreset.ToString();
         }
         else if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
             rotationPreset--;
-            pose.text = a + posePreset.ToString();
             rot.text = b + rotationPreset.ToString();
         }
-
-        webcamTex = (WebCamTexture)rawImage.material.mainTexture;
-        texture2D = new Texture2D(webcamTex.width, webcamTex.height);
-        texture2D.SetPixels32(webcamTex.GetPixels32());
-        Data data = new Data();
-        data.image = texture2D.EncodeToJPG();
-        string json = JsonUtility.ToJson(data);
-        StartCoroutine(PostRequest("http://127.0.0.1:5000/s", json));
     }
 
-    IEnumerator PostRequest(string url, string json)
+    void SetRingTransform(JSONNode json)
     {
-        var uwr = new UnityWebRequest(url, "POST");
-        byte[] jsonToSend = new System.Text.UTF8Encoding().GetBytes(json);
-        uwr.uploadHandler = (UploadHandler)new UploadHandlerRaw(jsonToSend);
-        uwr.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
-        uwr.SetRequestHeader("Content-Type", "application/json");
 
-        yield return uwr.SendWebRequest();
-
-        if (uwr.isNetworkError || uwr.isHttpError)
-        {
-            Debug.Log("Error While Sending: " + uwr.error);
-        }
-        else if (uwr.downloadHandler.text != null)
-        {
-            Debug.Log(uwr.downloadHandler.text);
-            var json1 = JSON.Parse(uwr.downloadHandler.text);
-
-            var pos = json1["positions"][posePreset];
-            var rot = json1["rotations"][rotationPreset];
-            var rect = canvas.GetComponent<RectTransform>().rect;
-            ring.localPosition = new Vector3(pos[0] * rect.width / 2, pos[1] * rect.height / 2, 0);
-            ring.localRotation = new Quaternion(rot[0], rot[1], rot[2], rot[3]);
-        }
+        var pos = json["positions"][posePreset];
+        var rot = json["rotations"][rotationPreset];
+        var rect = canvas.GetComponent<RectTransform>().rect;
+        ring.localPosition = new Vector3(pos[0] * rect.width / 2, pos[1] * rect.height / 2, 0);
+        ring.localRotation = new Quaternion(rot[0], rot[1], rot[2], rot[3]);
     }
 
-    private float Lerp(float x1, float x2, float x3, float y1, float y2)
+    public void Quit()
     {
-        return ((x2 - x3) * y1 + (x3 - x1) * y2) / (x2 - x1);
-    }
-
-    public void da()
-    {
+        SocketClose();
         Application.Quit();
     }
 }
